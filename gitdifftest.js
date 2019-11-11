6694e2bbe6c28ca05a5fb18a3ad95a3a4257d649
wangcong <wangcong@mintq.com>
2019-11-11 10:56:20 +0800

    fix:test

--- gitdifftest.js
+++ gitdifftest.js
@@ -11,9 +11,21 @@ const exec = require('child_process').exec
     const fin = nstr.slice(6, nstr.length - 1)
     const tems = []
     for (let i = 0; i < fin.length; i++) {
-      if (!fin[i].includes('.scss') && !fin[i].includes('interface') && !fin[i].includes('.test.') && fin[i].includes('src')) {
+      if (
+        !fin[i].includes('.scss') &&
+        !fin[i].includes('interface') &&
+        !fin[i].includes('.test.') &&
+        fin[i].includes('src')
+      ) {
         const src = './coverage/lcov-report/' + fin[i] + '.html'
-        const content = (await fs.readFileSync(src)).toString().split('\n')[27]
+        let content
+        try {
+          // 如果读取文件
+          content = (await fs.readFileSync(src)).toString().split('\n')[27]
+        } catch (error) {
+          console.log(error)
+          // exec('echo error > temp.txt')
+        }
         const branchRadio = content.match(/<span class="strong">(.+)<\/span>/)[1]
         tems.push([fin[i], branchRadio])
       }
