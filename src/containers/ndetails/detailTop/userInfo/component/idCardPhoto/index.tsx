7a253ea02c9b7648118d9c63f09f7356bb5f068d
Bonnie-lu <867454489@qq.com>
2019-11-15 18:26:58 +0800

    提交

--- src/containers/ndetails/detailTop/userInfo/component/idCardPhoto/index.tsx
+++ src/containers/ndetails/detailTop/userInfo/component/idCardPhoto/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './index.scss'
-import { imgPath } from 'global/constants'
+// import { imgPath } from 'global/constants'
 
 interface Props {
   config?: any[]
@@ -15,27 +15,27 @@ interface ItemType {
 
 class IdCardPhoto extends Component<Props> {
   render() {
-    const { config = [] } = this.props
+    // const { config = [] } = this.props
     return (
       <div className="identification-photo-wrapper">
-        {config.map((item, index) => {
+        {/* {config.map((item, index) => {
           return this.renderImgaes(item, index)
-        })}
+        })} */}
       </div>
     )
   }
-  renderImgaes = (item: ItemType, index: number): JSX.Element => {
-    const { showPicture } = this.props
-    const src = item.src.indexOf('/') === 0 ? item.src : imgPath + item.src
-    return (
-      <div className="photo-item" key={index}>
-        <div className="phont-item-wrap">
-          <img src={src} onClick={showPicture && showPicture(item.showId, src)} alt="hello world" />
-          <span className="photo-span">{item.title}</span>
-        </div>
-      </div>
-    )
-  }
+  // renderImgaes = (item: ItemType, index: number): JSX.Element => {
+  //   const { showPicture } = this.props
+  //   const src = item.src.indexOf('/') === 0 ? item.src : imgPath + item.src
+  //   return (
+  //     <div className="photo-item" key={index}>
+  //       <div className="phont-item-wrap">
+  //         <img src={src} onClick={showPicture && showPicture(item.showId, src)} alt="hello world" />
+  //         <span className="photo-span">{item.title}</span>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
 
 export default IdCardPhoto
